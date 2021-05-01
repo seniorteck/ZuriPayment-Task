65436406dcd6a9a7355894366120b55ada407ed3
code_kareem <seniorteck4real@gmail.com>
2021-05-01 10:01:39 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let age  = prompt('what is your age')
+
+if((age>=35) && (age.includes(3))){
+    console.log("welcome you qualify");
+} else {
+    console.log('thanks for applying try again later')
+}
